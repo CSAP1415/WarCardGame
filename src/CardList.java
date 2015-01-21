918ffb991e8883d477c30d64d8cf020bf4c7ffe0
CSAP1415 <phxbains@yahoo.com>
2015-01-20 17:31:22 -0700

    Update CardList.java

--- src/CardList.java
+++ src/CardList.java
@@ -1,35 +1,54 @@
-// create class CardList
-
- import java.util.*;
+import java.util.*;
 public class CardList
 {
- private ArrayList<Card> cards;
- private Random random=new Random();
- public CardList()
- {
- cards=new ArrayList<Card>();
- }
- public int getSize()
- {
- return cards.size();
- }
- public void addCardToBottom(Card card)
- {
- cards.add(card);
- }
- public void addCardToTop(Card card)
- {
- cards.add(0,card);
- }
- public void takeCardFromTop()
- {
- cards.remove(0);
- }
- public Card removeRandomCard()
- {
- int randCardNum=random.nextInt(cards.size());
- cards.remove(cards.get(randCardNum));
- return cards.get(randCardNum);
- }
+  private ArrayList<Card> cards=new ArrayList<Card>(52);
+  private Random random=new Random();
+  public CardList()
+  {
+    for(int i=1;i<=4;i++)
+    {
+      for(int x=1;x<=26;x++)
+      {
+        Card tempCard=new Card(i,x);
+        cards.add(tempCard);
+      }
+    }
+  } 
+  public int size()
+  {
+    return cards.size();
+  }
+  public int getSize()
+  {
+    return cards.size();
+  }
+  public void addCardToBottom(Card card)
+  {
+    cards.add(card);
+  }
+  public void addCardToTop(Card card)
+  {
+    cards.add(0,card);
+  }
+  
+  public Card takeCardFromTop()
+  {
+    Card temp=cards.get(0);
+    cards.remove(0);
+    return temp;
+  }
+  public Card get(int location)
+  {
+    return cards.get(location);
+  }
+  public Card removeRandomCard()
+  {
+    return cards.remove(random.nextInt(cards.size()));
+  }
 }
+
+
+
+
+
 
